5cbf5376415d9064a8f649e119a52080058682bd
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2022-07-19 01:25:35 +0300

    Add files via upload

---
+++ page_1_unpack_sequence.py
@@ -0,0 +1,12 @@
+
+p=(2,4)
+
+x,y=p
+
+print(x,y)
+
+print("---------------------------")
+data = [ 'ACME', 50, 91.1, (2012, 12, 21) ]
+name, shares, price, date = data
+
+print( name, shares, price, date )
